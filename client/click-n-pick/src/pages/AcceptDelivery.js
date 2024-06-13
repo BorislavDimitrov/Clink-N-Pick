4ea7b3925fc6b9d7b531214e15eed6de99563ec9
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2024-06-13 16:36:11 +0300

    Delete unnecessary input fields.

--- client/click-n-pick/src/pages/AcceptDelivery.js
+++ client/click-n-pick/src/pages/AcceptDelivery.js
@@ -174,31 +174,6 @@ function AcceptDelivery() {
                 placeholder="Order Number"
               />
             </div>
-            <div>
-              <label className="block text-sm font-medium text-gray-700">
-                CD Amount
-              </label>
-              <input
-                type="number"
-                name="CDAmount"
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                placeholder="CD Amount"
-              />
-            </div>
-            <div>
-              <label className="block text-sm font-medium text-gray-700">
-                CD Type
-              </label>
-              <select
-                name="CDType"
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              >
-                <option selected value="get">
-                  Get
-                </option>
-                <option value="give">Give</option>
-              </select>
-            </div>
 
             <div>
               <button
