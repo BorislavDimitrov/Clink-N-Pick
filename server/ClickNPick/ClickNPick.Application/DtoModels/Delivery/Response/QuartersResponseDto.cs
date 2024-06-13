2de85fa6a9490090381c11572a6a9754e2584118
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2024-06-13 20:15:43 +0300

    Create models needed to take quarters.

---
+++ server/ClickNPick/ClickNPick.Application/DtoModels/Delivery/Response/QuartersResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using ClickNPick.Application.DeliveryModels;
+using Newtonsoft.Json;
+
+namespace ClickNPick.Application.DtoModels.Delivery.Response
+{
+    public class QuartersResponseDto
+    {
+        [JsonProperty("quarters")]
+        public IEnumerable<Quarter>? Quarters { get; set; }
+    }
+}
---
+++ server/ClickNPick/ClickNPick.Web/Models/Delivery/Response/QuartersResponseModel.cs
@@ -0,0 +1,20 @@
+﻿using ClickNPick.Application.DeliveryModels;
+using ClickNPick.Application.DtoModels.Delivery.Response;
+using Newtonsoft.Json;
+
+namespace ClickNPick.Web.Models.Delivery.Response
+{
+    public class QuartersResponseModel
+    {
+        [JsonProperty("quarters")]
+        public IEnumerable<Quarter>? Quarters { get; set; }
+
+        public static QuartersResponseModel FromQuartersResponseDto(QuartersResponseDto dto)
+        {
+            return new QuartersResponseModel
+            {
+                Quarters = dto.Quarters,
+            };
+        }
+    }   
+}
