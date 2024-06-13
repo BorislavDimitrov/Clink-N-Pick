afa5dc3fc3e17173eada5c225ba029c9ad77ed4e
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2024-06-13 16:57:02 +0300

    Make input field to accept real numbers.

--- client/click-n-pick/src/pages/AcceptDelivery.js
+++ client/click-n-pick/src/pages/AcceptDelivery.js
@@ -146,6 +146,7 @@ function AcceptDelivery() {
                 Weight
               </label>
               <input
+                step="0.01"
                 type="number"
                 name="Weight"
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
