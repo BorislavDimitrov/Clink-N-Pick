1b455ea0f60c898efc4c32030acdc2df87a3dab8
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2024-06-13 15:44:47 +0300

    Create new exception for not found shipment request.

---
+++ server/ClickNPick/ClickNPick.Application/Exceptions/Delivery/ShipmentRequestNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace ClickNPick.Application.Exceptions.Delivery
+{
+    public class ShipmentRequestNotFoundException : Exception
+    {
+        private const string DefaultMessage = "Shipment not found.";
+
+        public ShipmentRequestNotFoundException() : base(DefaultMessage) { }
+
+        public ShipmentRequestNotFoundException(string message) : base(message) { }
+    }
+}
