41e9a67eb57997f21f731066be04e08c5f19a8e2
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2024-06-16 19:18:37 +0300

    Create new models to track courier.

---
+++ server/ClickNPick/ClickNPick.Application/DeliveryModels/RequestCourierStatus.cs
@@ -0,0 +1,20 @@
+﻿using ClickNPick.Application.Enumerations;
+using Newtonsoft.Json;
+
+namespace ClickNPick.Application.DeliveryModels
+{
+    public class RequestCourierStatus
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("status")]
+        public RequestCourierStatusType Status { get; set; }
+
+        [JsonProperty("note")]
+        public string Note { get; set; }
+
+        [JsonProperty("reject_reason")]
+        public string RejectReason { get; set; }
+    }
+}
---
+++ server/ClickNPick/ClickNPick.Application/DeliveryModels/RequestCourierStatusResultElement.cs
@@ -0,0 +1,14 @@
+﻿using ClickNPick.Application.Common;
+using Newtonsoft.Json;
+
+namespace ClickNPick.Application.DeliveryModels
+{
+    public class RequestCourierStatusResultElement
+    {
+        [JsonProperty("status")]
+        public RequestCourierStatus Status { get; set; }
+
+        [JsonProperty("error")]
+        public Error Error { get; set; }
+    }
+}
