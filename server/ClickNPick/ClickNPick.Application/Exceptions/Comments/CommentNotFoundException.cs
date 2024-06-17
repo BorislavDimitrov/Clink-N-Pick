3b3c7c105288817f35d496fbbcf6cc3946a57667
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2024-06-17 16:48:06 +0300

    Create exception for not found Comment.

---
+++ server/ClickNPick/ClickNPick.Application/Exceptions/Comments/CommentNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace ClickNPick.Application.Exceptions.Comments
+{
+    public class CommentNotFoundException : Exception
+    {
+        private const string DefaultMessage = "Comment not found.";
+
+        public CommentNotFoundException() : base(DefaultMessage) { }
+
+        public CommentNotFoundException(string message) : base(message) { }
+    }
+}
