31f40acb005ddfb9d0794651e8d6694e482db089
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2024-06-17 16:41:07 +0300

    Create Comment component.

---
+++ client/click-n-pick/src/components/Comment.js
@@ -0,0 +1,41 @@
+const Comment = ({
+  comment,
+  replies,
+  addComment,
+  parentId = null,
+  currentUserId,
+}) => {
+  const createdAt = new Date(comment.createdAt).toLocaleDateString();
+  return (
+    <div key={comment.id} className="comment">
+      <div className="comment-image-container">
+        <img src="/user-icon.png" />
+      </div>
+      <div className="comment-right-part">
+        <div className="comment-content">
+          <div className="comment-author">{comment.username}</div>
+          <div>{createdAt}</div>
+        </div>
+
+        <div className="comment-actions"></div>
+
+        {replies.length > 0 && (
+          <div className="replies">
+            {replies.map((reply) => (
+              <Comment
+                comment={reply}
+                key={reply.id}
+                addComment={addComment}
+                parentId={comment.id}
+                replies={[]}
+                currentUserId={currentUserId}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Comment;
