704605568c43d759ff349df96b76a14c0659fec4
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2024-06-17 16:51:15 +0300

    Create endpoint for creating a new comment.

---
+++ server/ClickNPick/ClickNPick.Web/Controllers/CommentsController.cs
@@ -0,0 +1,33 @@
+﻿using ClickNPick.Application.Services.Comments;
+using ClickNPick.Web.Extensions;
+using ClickNPick.Web.Models.Comments.Request;
+using ClickNPick.Web.Models.Comments.Response;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClickNPick.Web.Controllers
+{
+    public class CommentsController : ApiController
+    {
+        private readonly ICommentsService _commentsService;
+
+        public CommentsController(ICommentsService commentsService)
+        {
+            _commentsService = commentsService;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateCommentRequestModel model)
+        {
+            var dto = model.ToCreateCommentRequestDto();
+            var userId = HttpContext.User.GetId();
+            dto.CreatorId = userId;
+
+            var result = await _commentsService.CreateAsync(dto);
+            var response = CreateCommentResponseModel.FromCreateCommentResponseDto(result);
+
+            return Ok(response);
+        }
+    }
+}
