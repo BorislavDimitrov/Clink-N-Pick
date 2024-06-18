47215f963c15ccaf161378014fa48f6ff9b04c12
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2024-06-18 12:46:40 +0300

    Create request dto for deleting a comment.

---
+++ server/ClickNPick/ClickNPick.Application/DtoModels/Comments/Request/DeleteCommentRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ClickNPick.Application.DtoModels.Comments.Request
+{
+    public class DeleteCommentRequestDto
+    {
+        public string UserId { get; set; }
+
+        public string CommentId { get; set; }
+    }
+}
