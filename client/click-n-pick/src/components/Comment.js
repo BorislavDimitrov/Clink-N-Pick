e43f36bb4a722c5a546a0e48ebb552896dec6836
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2024-06-18 12:42:13 +0300

    Fix calculating of the time and attach onclick event to for delete.

--- client/click-n-pick/src/components/Comment.js
+++ client/click-n-pick/src/components/Comment.js
@@ -2,17 +2,18 @@ const Comment = ({
   comment,
   replies,
   addComment,
+  deleteComment,
   parentId = null,
   currentUserId,
 }) => {
   const fiveMinutes = 300000;
-  const timePassed = new Date() - new Date(comment.createdAt) > fiveMinutes;
+  const timePassed = new Date() - new Date(comment.createdOn) > fiveMinutes;
 
   const canReply = Boolean(currentUserId);
   const canEdit = currentUserId === comment.creatorId && !timePassed;
   const canDelete =
     currentUserId === comment.creatorId && replies.length === 0 && !timePassed;
-  const createdAt = new Date(comment.createdOn).toLocaleDateString();
+  const createdOn = new Date(comment.createdOn).toLocaleDateString();
   return (
     <div key={comment.id} class="flex">
       <div class="flex-shrink-0 mr-3">
@@ -28,12 +29,16 @@ const Comment = ({
             {comment.creatorUsername}
           </a>
         </strong>{" "}
-        <span class="text-xs text-gray-400">{createdAt}</span>
+        <span class="text-xs text-gray-400">{createdOn}</span>
         <p class="text-lg">{comment.content}</p>
         <div className="flex flex-row gap-2 mt-4">
-          {canReply && <div class="text-sm">Reply</div>}
-          {canEdit && <div class="text-sm">Edit</div>}
-          {canDelete && <div class="text-sm">Delete</div>}
+          {canReply && <button class="text-sm">Reply</button>}
+          {canEdit && <button class="text-sm">Edit</button>}
+          {canDelete && (
+            <button onClick={() => deleteComment(comment.id)} class="text-sm">
+              Delete
+            </button>
+          )}
         </div>
         <div className="space-y-4">
           {replies.length > 0 && (
@@ -43,6 +48,7 @@ const Comment = ({
                   comment={reply}
                   key={reply.id}
                   addComment={addComment}
+                  deleteComment={deleteComment}
                   parentId={comment.id}
                   replies={[]}
                   currentUserId={currentUserId}
@@ -53,34 +59,6 @@ const Comment = ({
         </div>
       </div>
     </div>
-
-    // <div key={comment.id} className="comment">
-    //   <div className="comment-image-container">
-    //     <img src={comment.creatorImageUrl} alt="user profile" />
-    //   </div>
-    //   <div className="comment-right-part">
-    //     <div className="comment-content">
-    //       <div className="comment-author">{comment.creatorUsername}</div>
-    //       <div>{createdAt}</div>
-    //     </div>
-    //     {comment.content}
-    //     <div className="comment-actions"></div>
-    //     {replies.length > 0 && (
-    //       <div className="replies">
-    //         {replies.map((reply) => (
-    //           <Comment
-    //             comment={reply}
-    //             key={reply.id}
-    //             addComment={addComment}
-    //             parentId={comment.id}
-    //             replies={[]}
-    //             currentUserId={currentUserId}
-    //           />
-    //         ))}
-    //       </div>
-    //     )}
-    //   </div>
-    // </div>
   );
 };
 
