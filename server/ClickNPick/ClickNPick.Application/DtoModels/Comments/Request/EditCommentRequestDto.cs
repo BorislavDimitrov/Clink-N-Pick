e91e02fa1d4c3a0bc435b895b2de53141a308ada
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2024-06-18 23:02:08 +0300

    Create request model and request dto for editing comment.

---
+++ server/ClickNPick/ClickNPick.Application/DtoModels/Comments/Request/EditCommentRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ClickNPick.Application.DtoModels.Comments.Request
+{
+    public class EditCommentRequestDto
+    {
+        public string CommentId { get; set; }
+
+        public string Content { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
---
+++ server/ClickNPick/ClickNPick.Web/Models/Comments/Request/EditCommentRequestModel.cs
@@ -0,0 +1,19 @@
+﻿using ClickNPick.Application.DtoModels.Comments.Request;
+
+namespace ClickNPick.Web.Models.Comments.Request
+{
+    public class EditCommentRequestModel
+    {
+        public string CommentId { get; set; }
+
+        public string Content { get; set; }
+
+        public EditCommentRequestDto ToEditCommentRequestDto()
+        {
+            var dto = new EditCommentRequestDto();
+            dto.CommentId = this.CommentId;
+            dto.Content = this.Content;
+            return dto;
+        }
+    }
+}
